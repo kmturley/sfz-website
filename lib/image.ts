1d3e268c530e72838e680cb872bf1dc014d2f5af
Kim T <kimturley@gmail.com>
2022-12-08 21:54:27 -0800

    Fix build error

--- lib/image.ts
+++ lib/image.ts
@@ -1,9 +1,9 @@
 import { SyntheticEvent } from 'react';
-import { getBasePath } from '../lib/path';
+import { GetBasePath } from '../lib/path';
 
 export function imageError(event: SyntheticEvent) {
   const el = event.target as HTMLImageElement;
-  const fallback = `${getBasePath()}/images/plugin.png`;
+  const fallback = `${GetBasePath()}/images/plugin.png`;
   if (el.getAttribute('src') !== fallback) {
     el.setAttribute('src', fallback);
   }
