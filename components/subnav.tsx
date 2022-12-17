0d99ca4f6b4dc4be5182f51c58d4929f0fa80280
Kim T <kimturley@gmail.com>
2022-12-17 15:39:08 -0800

    Documentation subnav toggle show/hide

--- components/subnav.tsx
+++ components/subnav.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/components/subnav.module.css';
 import { GetBasePath, IsSelected } from '../lib/path';
 import { YamlDocument } from '../lib/types';
+import { useState } from 'react';
 
 export type SubNavGroup = {
   name: string;
@@ -12,26 +13,49 @@ type SubNavProps = {
   groups: SubNavGroup[];
 };
 
-const SubNav = ({ groups }: SubNavProps) => (
-  <div className={styles.subnav}>
-    {groups.map((group: SubNavGroup) => (
-      <div className={styles.subnavsection} key={group.name}>
-        <h6>{group.name}</h6>
-        <ul className={styles.menu}>
-          {group.items.map((item: YamlDocument) => (
-            <li className={styles.menuItem} key={item.slug.join('_')}>
-              <a
-                href={`${GetBasePath()}${group.root}${item.slug.join('/')}`}
-                className={`${styles.navItem} ${IsSelected(`${group.root}${item.slug}`) ? styles.navItemActive : ''}`}
-              >
-                {item.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ))}
-  </div>
-);
+const SubNav = ({ groups }: SubNavProps) => {
+  const [isOpen, setIsOpen] = useState<{[key: string]: boolean;}>({
+    '/documentation/getting-started/': IsSelected('/documentation/getting-started/'),
+    '/documentation/tutorials/': IsSelected('/documentation/tutorials/'),
+    '/documentation/syntax/': IsSelected('/documentation/syntax/'),
+  });
+
+  const toggleSection = (e: React.MouseEvent<HTMLHeadingElement>) => {
+    const id: string = (e.target as HTMLHeadingElement).dataset.id || '';
+    if (isOpen[id] === true) {
+      setIsOpen(isOpen => ({
+        ...isOpen,
+        [id]: false
+      }));
+    } else {
+      setIsOpen({
+        [id]: true
+      });
+    }
+    console.log('toggleSection', id, isOpen);
+  }
+  
+  return (
+    <div className={styles.subnav}>
+      {groups.map((group: SubNavGroup) => (
+        <div className={`${styles.subnavsection} ${isOpen[group.root] ? styles.subnavsectionActive : ''}`} key={group.name}>
+          <h6 onClick={toggleSection} data-id={group.root}>{group.name}</h6>
+          <ul className={styles.menu}>
+            {group.items.map((item: YamlDocument) => (
+              <li className={styles.menuItem} key={item.slug.join('_')}>
+                <a
+                  href={`${GetBasePath()}${group.root}${item.slug.join('/')}`}
+                  className={`${styles.navItem} ${IsSelected(`${group.root}${item.slug}`) ? styles.navItemActive : ''}`}
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default SubNav;
